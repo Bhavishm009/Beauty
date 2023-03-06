8304e5e21d3dc21f31fe8d549b9e5b355d329b56
Bhavishm009 <bhavishm009@gmail.com>
2023-03-06 18:46:56 +0530

    updatae

--- src/Pages/Services/Servicepage.js
+++ src/Pages/Services/Servicepage.js
@@ -25,10 +25,8 @@ function Servicepage() {
   var formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "INR",
-
-    // These options are needed to round to whole numbers if that's what you want.
-    minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
-    //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    minimumFractionDigits: 0, 
+   
   });
   
   const loadScript = (src) => {
@@ -101,7 +99,7 @@ function Servicepage() {
             ButtonText="Book Now"
             className={Style.btn}
             type="submit"
-            onClick={() => alert('Please LogIn/Register ')}
+            onClick={() => alert('Please Login/Register ')}
           /> } 
           </div>
         </div>
